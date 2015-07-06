ce2dfb7ffa1976449cb2adcf68b86dfe4c6a0444
Nicholas Emanuel <njemanuel01@gmail.com>
2015-07-06 15:30:47 -0500

    Added a conditional to test for positive values.

--- 06-if-statements/triangle_global.js
+++ 06-if-statements/triangle_global.js
@@ -8,9 +8,10 @@ side3 = parseFloat(prompt("Enter the first side of the triangle."));
 
 //Test to see if sides make a triangle and outputs result
 if ((isNaN(side1)) || (isNaN(side2)) || (isNaN(side3))) {
-  alert("Invalid entry.")
-} else {
-  if (((side1 + side2) <= side3) || ((side2 + side3) <= side1) || ((side1 + side3) <= side2)) {
+  alert("Invalid entry. Must enter numbers.")
+} else if ((side1 < 0) || (side2 < 0) || (side3 < 0)) {
+  alert("Sides must have a positive length.")
+} else if (((side1 + side2) <= side3) || ((side2 + side3) <= side1) || ((side1 + side3) <= side2)) {
     alert("Not a triangle.")
   } else {
     alert("It's a  triangle")
